90d26ce27d5f5bb6f77182c47bfe7c0b40d9aea7
nastya <hociy2015@gmail.com>
2026-02-23 13:26:48 +0200

    Add user profiles, viewsets, and follow/unfollow logic

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
+from .models import Profile
+
 User = get_user_model()
 
 
@@ -17,6 +19,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             username=validated_data["username"],
             password=validated_data["password"],
         )
+        Profile.objects.create(user=user)
         return user
 
 
@@ -24,3 +27,21 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("id", "email", "username")
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source="user.email")
+
+    class Meta:
+        model = Profile
+        fields = ("id", "user", "avatar", "bio")
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+    followers_count = serializers.IntegerField(read_only=True)
+    following_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "email", "username", "profile", "followers_count", "following_count")
--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
-from .views import UserRegistrationView
+from .views import UserRegistrationView, ProfileRetrieveUpdateView, UserViewSet
 
+router = DefaultRouter()
+router.register(r"", UserViewSet, basename="user")
 urlpatterns = [
     path("register/", UserRegistrationView.as_view(), name="register"),
-    path("login", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/profile/", ProfileRetrieveUpdateView.as_view(), name="my_profile"),
+    path("", include(router.urls)),
 ]
