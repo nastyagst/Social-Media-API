55c4bede774085144d4a286b7c60512952ee8614
nastya <hociy2015@gmail.com>
2026-02-24 19:39:01 +0200

    Extract secrets to environment variables and add .env.sample

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fzhykydobki1-b(9ot&am5wzf$zwn_qb$n=#-ixhyrcquvfua_"
+SECRET_KEY = os.environ.get("SECRET_KEY", "default-insecure-key-only-for-local-dev")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
